4b23e17d078ae3b7600d63da36b3b90e92d3a6e8
MaxHalleran <maxhalleran@gmail.com>
2019-10-22 11:56:06 -0600

    Fixing special character overwriting

--- main.js
+++ main.js
@@ -1,91 +1,90 @@
 const domReady = function () {
-	// Setting up variables
-	const dateVisual = document.getElementById('date-visual');
-	const dateValue = document.getElementById('date-value');
-	const cardVisual = document.getElementById('card-visual');
-	const cardValue = document.getElementById('card-value');
-	const button = document.getElementById('submit');
-
-	function validate(e) {
-		const maxLength = e.target.dataset.mask.length - 1;
-
-		if (e.key.charCodeAt() === 66) {
-			return;
-		}
-		if (e.key.charCodeAt() > 57 && e.key.charCodeAt() !== 127) {
-			e.preventDefault();
-			return false;
+	class Mask {
+		constructor(elementId, char, length, positions) {
+			this.elementId = elementId;
+			this.char = char;
+			this.length = length;
+			this.positions = positions;
 		}
 
-		if (e.key.charCodeAt() > 31 && e.key.charCodeAt() < 48) {
-			e.preventDefault();
-			return false;
-		}
-
-		if (e.target.value.length > maxLength && e.code.includes("Digit")) {
-			e.preventDefault();
-			return false;
-		}
-		
-	}
-
-	/**
-	 * dateMask
-	 * This function is called on input's looking to mask the date with the form XX/XX/XXXX
-	 * 
-	 * @param {Event} e - The event taking place
-	*/
-	function dateMask(e) {
-		if (e.target.value.length === 2 || e.target.value.length === 5) {
-			e.target.value += '/';
-		}
-	}
-
-	// The Card Mask function following XXXX XXXX XXXX XXXX
-	function cardMask(e) {
-		if (e.target.value.length === 4 || e.target.value.length === 9 || e.target.value.length === 14) {
-			e.target.value += ' ';
-		}
-	}
-
-	function standardMask(e, target, char) {
-		const previous = e.target.dataset.previous;
-
-		if (previous && previous[previous.length - 1] != char) {
-			if (e.target.dataset.type === "date") {
-				dateMask(e);
-			} else if (e.target.dataset.type === "card") {
-				cardMask(e);
+		validateKeyInput = (e) => {
+			if (e.key.charCodeAt() === 66 || e.key.charCodeAt() === 84) {
+				return;
+			}
+			if (e.key.charCodeAt() > 57 && e.key.charCodeAt() !== 127) {
+				e.preventDefault();
+				return false;
+			}
+			if (e.key.charCodeAt() > 31 && e.key.charCodeAt() < 48) {
+				e.preventDefault();
+				return false;
+			}
+			if (e.target.value.length > this.length - 1 && e.code.includes("Digit")) {
+				e.preventDefault();
+				return false;
 			}
 		}
 
-		e.target.dataset.previous = e.target.value;
-		target.value = e.target.value.replace(/[/\s]/g, "");
-	}
+		mask = (e) => {
+			let specialChar = false;
+			this.positions.forEach(position => {
+				if (e.target.value.length === position) {
+					specialChar = true;
+				}
+			});
+			e.target.value += specialChar ? this.char : '';
+		}
 
-	// Update the actual value in the hidden field
-	function updateValue(e) {
-		const target = document.getElementById(e.target.dataset.type + "-value");
-		if (e.target.dataset.type === "date") {
-			standardMask(e, target, "/");
-		} else if (e.target.dataset.type === "card") {
-			standardMask(e, target, " ");
-		} else {
-			console.log('Huh, something went wrong. Maybe there\'s an unspecified input element?');
+		validatePlacement = (e) => {
+			let specialCharPosition = false;
+			this.positions.forEach((position) => {
+				if (e.target.value.length === position) {
+					specialCharPosition = position;
+				}
+			})
+			return specialCharPosition;
+		}
+
+		// This controls the input event
+		updateValue = (e) => {
+			const previous = e.target.dataset.previous;
+
+			// We want to make sure the updated value wont be longer than the allowed amount
+			if (e.target.value.length > this.length) {
+				e.target.value = e.target.value.slice(0, this.length - 1);
+			}
+			// Let's deal with the 'erase special character than right problem
+			const validPosition = this.validatePlacement(e);
+			console.log(e);
+			if (validPosition && e.inputType !== "deleteContentBackward") {
+				e.target.value += this.char;
+			}
+
+			if (previous && previous[previous.length - 1] != this.char) {
+				this.mask(e);
+			}
+
+			e.target.dataset.previous = e.target.value;
+		}
+
+		initiate() {
+			document.getElementById(this.elementId).addEventListener('input', this.updateValue);
+			document.getElementById(this.elementId).addEventListener('keydown', this.validateKeyInput);
 		}
 	}
 
-	// Logging the submit button
-	function logSubmit(e) {
-		console.log("Date Value: ", dateValue.value);
-		console.log("Card Value: ", cardValue.value);
-	}
+	const dateMask = new Mask('date', '/', 10, [2, 5]);
+	const cardMask = new Mask('card', ' ', 19, [4, 9, 14]);
 
-	// Attaching events
-	dateVisual.addEventListener('input', updateValue);
-	dateVisual.addEventListener('keydown', validate);
-	cardVisual.addEventListener('input', updateValue);
-	cardVisual.addEventListener('keydown', validate);
+	dateMask.initiate();
+	cardMask.initiate();
+
+	// Logging the submit button
+	const button = document.getElementById('submit');
+	function logSubmit(e) {
+		console.log("Date Value: ", dateVisual.value);
+		console.log("Card Value: ", cardVisual.value.replace(/[/\s]/g, ""));
+	}
 	button.addEventListener('click', logSubmit);
 };
 
--- readme.md
+++ readme.md
@@ -5,3 +5,32 @@
 The function is simple, on the keypress, it determines what the key is and whether it should be allowed or rejected. If it's allowed, it counts the number of characters in the string and if the last interaction was not a 'special' character being deleted and the current value is an appropriate length, it will add the 'special' character.
 
 I used two inputs, one for visuals and the other for the actual value, but this is not completely necessary. As we're interacting with actual keypresses, we can determine exactly what keys should have an effect. The reason I went with two for each value is that the 'front facing' input is just a text input whereas the 'background' input is a number input, only allowing numbers.
+
+## 
+
+* Slash error
+
+* autofill
+
+* Refactor
+
+Refactor / Loss of character issue addression
+
+The flow goes:
+* user inputs data
+	* If it's a typed character and not pasted/autocompleted, we evaluate the character
+	* Then we evaluate what the final result would be and format it
+
+Needed information:
+* The input value
+* The replacement character
+	* Technically contained in the data mask but we will provide them ourselves
+* The positions of replacement
+	* Technically contained in the data mask but we will provide them ourselves
+* Target max string length
+	* Contained in the data-mask value ie. the length of the mask
+
+
+We dont need to worry about string length as of yet. Let's find a way to evaluate strings and reformat them if they're not valid.
+
+So as we're validating, we need to take into account the fact that the strings can be one larger according to the mask.
